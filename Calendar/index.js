c95d9b4650cd1fa35747c1aa75233cb5055eae86
AndreyChugunovAC1 <76919702+AndreyChugunovAC1@users.noreply.github.com>
2021-06-17 10:47:58 +0300

    Update index.js

--- Calendar/index.js
+++ Calendar/index.js
@@ -7,46 +7,50 @@ prev = (a) => {return a == 0 ? 11 : a - 1};
 function Fil(y, m) {
     Dt = new Date(y, m, 1);
     n = (Dt.getDay() + 6) % 7;
-    if (m == 2) {
-
+    L = dim[prev(m)];
+    for (var i = 1; i < 42; i++)
+    {
+        document.getElementById(String(i)).style = "color:green"
     }
-    else 
-        L = dim[prev(m)];
+    if (m == 2 && (y % 400 == 0 || (y % 100 != 0 && y % 4 == 0))) {
+        L++;
+    }
+    document.getElementById("month").value = m;
+    document.getElementById("year").value = y;    
     for (var i = 1; i <= n; i++) {
         document.getElementById(String(i)).innerHTML = L - n + i;
-        document.getElementById(String(i)).classList = "tmp";
+        document.getElementById(String(i)).style = "color:grey";
     }
     S = dim[m];
-    if (y % 400 == 0 || (y % 100 != 0 && y % 4 == 0))
+    if (y % 400 == 0 || (y % 100 != 0 && y % 4 == 0) && m == 1)
         S = 29;
     for (var i = 1; i <= S; i++) {
         document.getElementById(String(n + i)).innerHTML = i;
         document.getElementById(String(i + n)).classList= "t1";
     } 
-    if (S == 28 && n == 0) {
-        for (var i = 29; i <= 42; i++) {
+    F = S + n;
+    if (F > 35)
+        for (var i = F + 1; i <= 42; i++) {
+            document.getElementById(String(i)).innerHTML = i - F;
+            document.getElementById(String(i)).classList= "tmp";
+            document.getElementById(String(i)).style = "color:grey";
+        }
+    else if (F > 28) {
+        for (var i = F + 1; i <= 35; i++) {
+            document.getElementById(String(i)).innerHTML = i - F;
+            document.getElementById(String(i)).style = "color:grey";
+        }
+        for (var i = 36; i <= 42; i++) {
             document.getElementById(String(i)).innerHTML = "";
-            document.getElementById(String(i)).classList = "empty";
+            document.getElementById(String(i)).classList= "empty";
         }
     }
-    else {
-        for (var i = n + S + 1; i <= 35; i++) {
-            document.getElementById(String(i)).innerHTML = i - n - S;
-            document.getElementById(String(i)).classList = "tmp";
-        }
-        if (n + S <= 36) {
-            for (var i = 36; i <= 42; i++) {
-                document.getElementById(String(i)).innerHTML = "";
-                document.getElementById(String(i)).classList = "empty"
-            }
-        }
-        else {
-            for (var i = 36; i <= 42; i++) {
-                document.getElementById(String(i)).innerHTML = i - 34;
-                document.getElementById(String(i)).classList = "tmp";
-            }
-        }
+    Now = new Date;
+    if (Now.getFullYear() == y && Now.getMonth() == m) {
+        document.getElementById(String(n + Now.getUTCDate())).style = "color:red";
     }
+
+
     /* document.getElementById("month").value = m; */
     /*
     for (var i = 1; i <= 42; i++)
@@ -56,28 +60,31 @@ function Fil(y, m) {
 }
 
 function Plus() {
-    x = parseInt(document.getElementById("month").value);
-    if (x != 11) {
-        x++
+    m = parseInt(document.getElementById("month").value);
+    y = parseInt(document.getElementById("year").value);
+    if (m == 11) {
+        y++; 
+        m = 0;
     }
-    else if (x == 11 && parseInt(document.getElementById("year").value != 9999) ) {
-        x = 0, document.getElementById("year").value = String(parseInt(document.getElementById("year").value) + 1);
+    else {
+        m++;
     }
-    Fil(parseInt(document.getElementById("year").value), x);
-    document.getElementById(String("month")).value = x;
+    Fil(y, m);
 }
 
 function Minus() {
-    x = parseInt(document.getElementById("month").value);
-    if (x != 0) {
-        x--
+    m = parseInt(document.getElementById("month").value);
+    y = parseInt(document.getElementById("year").value);
+    if (m == 0) {
+        y--; 
+        m = 11;
     }
-    else if (x == 0 && parseInt(document.getElementById("year").value != 1) ) {
-        x = 11, document.getElementById("year").value = String(parseInt(document.getElementById("year").value) - 1);
+    else {
+        m--;
     }
-    Fil(parseInt(document.getElementById("year").value), x);
-    document.getElementById(String("month")).value = x;
+    Fil(y, m);
 }
+
 
 Dt = new Date();
 Fil(Dt.getFullYear(), Dt.getMonth());
